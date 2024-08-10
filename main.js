216a4dea2a1a951d53e460cfd4061ff1eea46785
seb <13894287+Sebamor@users.noreply.github.com>
2024-08-10 04:34:25 -0700

    Added Read and Delete Functionality

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const library = [];
 
 // Book Functions
-function Book(title, author, genre, pages) {
+function Book(title, author, genre, pages, read) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.pages = pages;
+    this.read = read;
 }
 
-function addBook(title, author, genre, pages) {
+function addBook(title, author, genre, pages, read) {
     deleteBookList();
 
-    const newBook = new Book(title, author, genre, pages);
+    const newBook = new Book(title, author, genre, pages, read);
     library.push(newBook);
     displayBooks(library);
 }
@@ -20,7 +21,47 @@ function displayBooks(array) {
     const bookList = document.getElementById('bookList');
     library.forEach((book) => {
         let li = document.createElement('li');
-        li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}`;
+        li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}, Read: ${book.read}`;
+
+        if (book.read === true) {
+            li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}, Read: &check;`;
+        }
+        else {
+            li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}, Read: &times;`;
+        }
+
+        let toggleReadButton = document.createElement('button');
+        toggleReadButton.textContent = 'Toggle';
+        toggleReadButton.addEventListener('click', () => {
+            book.read = !book.read;
+            if (book.read === true) {
+                li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}, Read: &check;`;
+                li.appendChild(toggleReadButton);
+                li.appendChild(deleteButton);
+            }
+            else {
+                li.innerHTML = `Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}, Pages: ${book.pages}, Read: &times;`;
+                li.appendChild(toggleReadButton);
+                li.appendChild(deleteButton);
+            }
+        });
+        li.appendChild(toggleReadButton);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        deleteButton.dataset.title = book.title;
+
+        deleteButton.addEventListener('click', (e) => {
+            const title = e.target.dataset.title;
+            const index = library.findIndex((book) => book.title === title);
+            if (index !== -1) {
+                library.splice(index, 1);
+                e.target.parentElement.remove();
+            }
+        });
+
+        li.appendChild(deleteButton);
         bookList.appendChild(li);
     })
 }
@@ -57,9 +98,12 @@ const nTitle = document.getElementById('bookTitle');
 const nAuthor = document.getElementById('bookAuthor');
 const nGenre = document.getElementById('bookGenre');
 const nPages = document.getElementById('bookPages');
-bookSubmitButton.addEventListener('click', () => {
-    addBook(nTitle.value, nAuthor.value, nGenre.value, nPages.value);
+const nRead = document.getElementById('bookRead');
+bookSubmitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBook(nTitle.value, nAuthor.value, nGenre.value, nPages.value, nRead.checked);
     modal.style.display = 'none';
     bookForm.reset();
-
 })
+
+addBook('Test Book', 'Test Author', 'Test Genre', 100, true);
